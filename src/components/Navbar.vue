0b686807954d54f7ce921b184d2fffb548484b21
Fakhri-8G <sideques1@gmail.com>
2026-09-17 10:47:01 +0700

    Create basic navbar and app structure

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav>
+        <a href="#">Fakhri</a>
+
+        <div>
+            <a href="#">About</a>
+            <a href="#">Skills</a>
+            <a href="#">Projects</a>
+            <a href="#">Contact</a>
+        </div>
+    </nav>
+</template>
